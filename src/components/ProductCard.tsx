5f600229e738ec5864261b52579e625caa430ec1
PIVIIII <lenvariya@gmail.com>
2024-03-14 18:40:56 +0700

    a5

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,19 @@
-import styles from './productcard.module.css'
 import Image from 'next/image';
+import InteractiveCard from './InteractiveCard';
+
 
 export default function ProductCard ( { carName, imgSrc } : {carName:string, imgSrc:string }) {
+
     return (
-        <div className='w-1/5 h-[300px] roundded-lg shadow-lg'>
+        <InteractiveCard constentName = {carName}>    
             <div className='w-full h-[70%] relative round-t-lg'> 
-                <Image src={imgSrc}
+            <Image src={imgSrc}
                 alt='Product Picture'
                 fill={true}
                 className='object-cover rounded-t-lg'
                 />
             </div>
             <div className='w-full h-[30%] p-[10px]'>{carName}</div>
-        </div>
+        </InteractiveCard>
     );
 }
