64619e716bb08ffd365af3b02ee206aaba73f445
PIVIIII <lenvariya@gmail.com>
2024-03-20 11:01:29 +0700

    a10

--- src/app/reservations/manage/@dashboard/page.tsx
+++ src/app/reservations/manage/@dashboard/page.tsx
@@ -1,7 +1,23 @@
-export default function DashboardPage() {
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import getUserProfile from "@/libs/getUserProfile"
+
+export default async function DashboardPage() {
+    const session = await getServerSession(authOptions)
+    if(!session||!session.user.token) return null
+
+    const profile = await getUserProfile(session.user.token)
+    var createAt = new Date(profile.data.createAt)
+            
     return (
-        <main className="bg-slate-100">
-            <div>Your Dashboard</div>
+        <main className="bg-slate-100 m-5 p-5">
+            <div className="text-2xl"></div>
+            <table className="table-auto border-seperate border-spacing-2"><tbody>
+                <tr><td>Email</td><td>{profile.data.email}</td></tr>
+                <tr><td>Tel</td><td>{profile.data.tel}</td></tr>
+                <tr><td>Member Since</td><td>{createAt.toString()}</td></tr>
+            </tbody>
+            </table>
         </main>
     )
 }
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,14 +1,29 @@
 import styles from './topmenu.module.css'
 import Image from 'next/image';
 import TopMenuItem from './TopMenuItem';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import Link from 'next/link';
 
-export default function Topmenu() {
+export default async function Topmenu() {
+    const session = await getServerSession(authOptions)
+
     return (
         <div className={styles.menucontainer}> 
             <Image src={'/img/logo.png'} className={styles.logoimg} alt='logo'
             width={0} height={0} sizes='100vh'/>
             <TopMenuItem title='Reservations' pageRef='/reservations'/>
-            <TopMenuItem title='About' pageRef='/about'/>            
+            <TopMenuItem title='About' pageRef='/about'/>   
+
+            {
+                session? <Link href="api/auth/signout">
+                    <div className='flex items-center absolute right-0 h-full px-2 text-cyan-600 text-sm'>Sigh-Out of {session.user?.name}</div>
+                </Link>
+                :  <Link href="api/auth/signin">
+                    <div className='flex items-center absolute right-0 h-full px-2 text-cyan-600 text-sm'>
+                    Sigh-In</div>
+                </Link>
+            }         
         </div>
     );
 }
